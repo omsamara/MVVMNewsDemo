84595185d4ba5d41e18f89b61825444ff3e2f13b
Omar Samara <omsamara@ncsu.edu>
2020-05-03 18:47:39 -0400

    Navigation set up with 3 fragments and back stack management

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
@@ -2,6 +2,9 @@ package com.androiddevs.mvvmnewsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.findFragment
+import androidx.navigation.NavHost
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.androiddevs.mvvmnewsapp.R
@@ -13,6 +16,17 @@ class NewsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
 
-        bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.newsNavHostFragment) as NavHostFragment
+        bottomNavigationView.setupWithNavController(navHostFragment.navController)
+
+        // If we replaced the FragmentViewContainer with a FrameLayout and a fragment, the code will
+        // look like this.
+
+        // bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
+
+        // This is because newsNavHostFragment.findNavController() relies on the Fragment's View
+        // to already be created which isn't the case when using FragmentContainerView
+        // (as it uses a FragmentTransaction under the hood to add the NavHostFragment).
+        // Therefore, outside of onCreate() we can use findNavController()
     }
 }
