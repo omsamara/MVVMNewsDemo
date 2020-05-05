dd1cfbefe2b996529cd724f0336c0b00d77688f0
Omar Samara <omsamara@ncsu.edu>
2020-05-05 19:47:30 -0400

    Added retrofit base setup

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "037f6a26ac50426b95af8e4e6b4d9d51"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
